f79d1ff5993fd8650ec205c82afcb20e4a19fc8e
meishiguang <yyongtai@163.com>
2014-05-16 18:29:00 +0800

    init meitang

---
+++ meitang/__init__.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from flask import Flask, request, render_template
+#from flask.ext.babel import Babel
+
+from .config import DefaultConfig
+#from .api import api
+from .extensions import db 
+
+
+# For import *
+__all__ = ['create_app']
+
+DEFAULT_BLUEPRINTS = (
+    shai,
+    api,
+)
+
+
+"""Create a Flask app."""
+
+app_name = DefaultConfig.PROJECT
+blueprints = DEFAULT_BLUEPRINTS
+
+#app = Flask(app_name, instance_path=INSTANCE_FOLDER_PATH, instance_relative_config=True)
+app = Flask(app_name)
+configure_app(app, config)
+configure_hook(app)
+configure_blueprints(app, blueprints)
+configure_extensions(app)
+configure_logging(app)
+configure_template_filters(app)
+configure_error_handlers(app)
+
+
+def configure_app(app, config=None):
+    """Different ways of configurations."""
+
+    # http://flask.pocoo.org/docs/api/#configuration
+    app.config.from_object(DefaultConfig)
+
+    # http://flask.pocoo.org/docs/config/#instance-folders
+    #app.config.from_pyfile('production.cfg', silent=True)
+
+    if config:
+        app.config.from_object(config)
+
+
+
+def configure_extensions(app):
+    # flask-sqlalchemy
+    db.init_app(app)
+
+
+def configure_blueprints(app, blueprints):
+    """Configure blueprints in views."""
+
+    for blueprint in blueprints:
+        app.register_blueprint(blueprint)
+
+
+def configure_template_filters(app):
+    pass
+
+
+def configure_logging(app):
+    """Configure file(info) and email(error) logging."""
+
+    if app.debug or app.testing:
+        # Skip debug and test mode. Just check standard output.
+        return
+
+    import logging
+
+    # Set info level on logger, which might be overwritten by handers.
+    # Suppress DEBUG messages.
+    app.logger.setLevel(logging.INFO)
+
+    info_log = os.path.join(app.config['LOG_FOLDER'], 'info.log')
+    info_file_handler = logging.handlers.RotatingFileHandler(info_log, maxBytes=100000, backupCount=10)
+    info_file_handler.setLevel(logging.INFO)
+    info_file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s '
+        '[in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.addHandler(info_file_handler)
+
+
+
+def configure_hook(app):
+    @app.before_request
+    def before_request():
+        pass
+
+
+def configure_error_handlers(app):
+
+    @app.errorhandler(403)
+    def forbidden_page(error):
+        return render_template("errors/forbidden_page.html"), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("errors/page_not_found.html"), 404
+
+    @app.errorhandler(500)
+    def server_error_page(error):
+        return render_template("errors/server_error.html"), 500
---
+++ meitang/config.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from .utils import make_dir 
+
+PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
+class BaseConfig(object):
+
+    PROJECT = "meitang"
+
+    # Get app root path, also can use flask.root_path.
+    # ../../config.py
+    #PROJECT_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+    DEBUG = False
+    TESTING = False
+
+    #ADMINS = ['youremail@yourdomain.com']
+
+    # http://flask.pocoo.org/docs/quickstart/#sessions
+    SECRET_KEY = 'secret key'
+
+    LOG_FOLDER = os.path.join(PROJECT_ROOT, 'logs')
+    make_dir(LOG_FOLDER)
+
+
+class DefaultConfig(BaseConfig):
+
+    DEBUG = True
+
+    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + PROJECT_ROOT + '/production.sqlite'
+    # MYSQL for production.
+    #SQLALCHEMY_DATABASE_URI = 'mysql://username:password@server/db?charset=utf8'
+
+    # Flask-babel: http://pythonhosted.org/Flask-Babel/
+    #ACCEPT_LANGUAGES = ['zh']
+    #BABEL_DEFAULT_LOCALE = 'en'
+
+
+class TestConfig(BaseConfig):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + PROJECT_ROOT + '/test.sqlite'
